65007389a27463dc611f220175a85acbd355a9f4
Marius Luexmann <m.luexmann@gmail.com>
2024-10-13 19:35:35 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,128 @@
+# Endless Cron Commands for Symfony
+
+This package is a small extension of [wrep/daemonizable-command](https://packagist.org/packages/wrep/daemonizable-command). If no Upstart or systemd are available to set up a daemon, a simple cronjob and Symfony Lock are sufficient to produce an almost similar result.
+
+## Detailed information on Daemonizable / Endless Commands
+Check out the documentation of wrep/daemonizable-command for more information on how to create endless commands: [wrep/daemonizable-command](https://packagist.org/packages/wrep/daemonizable-command)
+
+## Installation
+
+`composer require mluex/endless-cron-command`
+
+### Which version to use?
+* Version 5.* for Symfony 7 and higher
+* Version 4.* for Symfony 6
+
+## Usage
+
+### 1. Configuration
+EndlessCronCommands require symfony/lock to ensure that only one instance is executed at a time. Please configure the bundle accordingly in your .env.local, e.g.
+    
+```dotenv
+###> symfony/lock ###
+LOCK_STORE_DSN=semaphore:acme
+###< symfony/lock ###
+```
+
+⚠️ Make sure to use a lock store that supports expiry! Check the [Symfony Lock documentation](https://symfony.com/doc/current/components/lock.html) for more information.
+
+### 2. Implementation
+
+Create a Symfony command that extends `EndlessCronCommand` and off you go. Here is a minimal example:
+
+```php
+namespace Acme\DemoBundle\Command;
+
+use Mluex\EndlessCronCommand\EndlessCronCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MinimalDemoCommand extends EndlessCronCommand
+{
+    // This is just a normal Command::configure() method
+    protected function configure(): void
+    {
+        $this->setName('acme:minimaldemo')
+            ->setDescription('An EndlessCronCommand implementation example');
+    }
+
+    // Method will be called in an endless loop
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        // Do some work
+        file_put_contents('/tmp/acme-timestamp.txt', time());
+    }
+}
+```
+
+If your command includes Monolog's FingersCrossed Log Handlers, you may want to clear them after every iteration as wrep pointed out in his [documentation](https://github.com/mac-cain13/daemonizable-command?tab=readme-ov-file#memory-usage-and-leaks). EndlessCronCommand provides a method for this:
+
+```php
+namespace Acme\DemoBundle\Command;
+
+use Mluex\EndlessCronCommand\EndlessCronCommand;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MinimalDemoCommand extends EndlessCronCommand
+{
+    public function __construct(
+        private readonly LoggerInterface $logger,
+        string $name = null
+    ) {
+        parent::__construct($name);
+    }
+
+    // This is just a normal Command::configure() method
+    protected function configure(): void
+    {
+        $this->setName('acme:minimaldemo')
+            ->setDescription('An EndlessCronCommand implementation example');
+    }
+
+    // Method will be called in an endless loop
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        // Do some work
+        file_put_contents('/tmp/acme-timestamp.txt', time());
+        
+        // Clear FingersCrossed Log Handlers
+        $this->clearFingersCrossedLogHandlers($this->logger);
+    }
+}
+```
+    
+
+### 3. Cronjob setup
+
+Add a cronjob to your crontab to run the command every minute.
+
+```bash
+* * * * * php /path/to/your/project/bin/console acme:minimaldemo >/dev/null 2>&1
+```
+
+### 4. Advanced usage
+
+It may make sense to limit the runtime of an instance and restart the command regularly. Use the runtime option for this:
+
+```bash
+* * * * * php /path/to/your/project/bin/console acme:minimaldemo --runtime=600 >/dev/null 2>&1
+```
+
+In order to run the command's execute() method more often in the main loop (default is 5 sec), you can use the frequency option to set a lower timeout between iterations (1 sec in this example):
+
+```bash
+* * * * * php /path/to/your/project/bin/console acme:minimaldemo --frequency=1 >/dev/null 2>&1
+```
+
+Every iteration will refresh the lock and push its expiry time further into the future. If the command crashes, the lock will expire after the lock-ttl time and the next cronjob will start a new instance. You can set the lock-ttl to a lower value to ensure that the command is restarted more quickly after a crash:
+
+```bash
+* * * * * php /path/to/your/project/bin/console acme:minimaldemo --lock-ttl=60 >/dev/null 2>&1
+```
+
+## TODO
+- [ ] Examples
+- [ ] Make sure lock store supports expiry
+- [ ] Unit Tests
---
+++ src/Command/EndlessCronCommand.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace Mluex\EndlessCronCommand\Command\EndlessCronCommand;
+
+use RuntimeException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Lock\LockFactory;
+use Symfony\Component\Lock\LockInterface;
+use Wrep\Daemonizable\Command\EndlessCommand;
+
+class EndlessCronCommand extends EndlessCommand
+{
+    private const DEFAULT_LOCK_TTL = 60.0;
+
+    private ?LockInterface $lock = null;
+    private ?int $startTime = null;
+    private ?int $runtime = null;
+    private ?int $lockTtl = null;
+
+    public function __construct(
+        private LockFactory $lockFactory,
+        string $name = null
+    ) {
+        parent::__construct($name);
+
+        $this->addOption(
+            'runtime',
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'Time (sec) after which the command will gracefully shutdown.'
+        );
+
+        $this->addOption(
+            'frequency',
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'Frequency (sec) of the main loop.',
+            EndlessCommand::DEFAULT_TIMEOUT
+        );
+
+        $this->addOption(
+            'lock-ttl',
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'TTL (sec) of the lock.',
+            self::DEFAULT_LOCK_TTL
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function starting(InputInterface $input, OutputInterface $output): void
+    {
+        $this->startTime = time();
+
+        $frequency = $input->getOption('frequency');
+        if (!is_numeric($frequency) || (int) $frequency < 1) {
+            throw new RuntimeException('Frequency must be a positive number.');
+        }
+        $this->setTimeout((float) $frequency);
+
+        $runtime = $input->getOption('runtime');
+        if (null !== $runtime && (!is_numeric($runtime) || $runtime < 1)) {
+            throw new RuntimeException('Runtime must be a positive number or empty.');
+        }
+        $this->runtime = null !== $runtime ? (int) $runtime : null;
+
+        $lockTtl = $input->getOption('lock-ttl');
+        if (!is_numeric($lockTtl) || (int) $lockTtl < 1) {
+            throw new RuntimeException('Lock TTL must be a positive number.');
+        }
+        $this->lockTtl = (float) $lockTtl;
+
+        parent::starting($input, $output);
+    }
+
+    protected function die(OutputInterface $output, string $msg): int
+    {
+        $this->shutdown();
+        $this->releaseLock();
+
+        $this->setReturnCode(Command::FAILURE);
+
+        return $this->failedIteration($output, $msg);
+    }
+
+    protected function failedIteration(OutputInterface $output, ?string $msg = null): int
+    {
+        if (null !== $msg && $output->getVerbosity() > OutputInterface::VERBOSITY_QUIET) {
+            $output->writeln(sprintf('<error>%s</error>', $msg));
+        }
+
+        return Command::FAILURE;
+    }
+
+    /**
+     * @noinspection PhpUnused
+     */
+    protected function successfulIteration(OutputInterface $output): int
+    {
+        return Command::SUCCESS;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function startIteration(InputInterface $input, OutputInterface $output): void
+    {
+        /*
+         * It would be cleaner to catch any lock related exceptions here and shutdown the command properly by calling
+         * die() method. But unfortunately startIteration can only stop the command by throwing an exception.
+         */
+        $this->acquireLock($input);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function finishIteration(InputInterface $input, OutputInterface $output): void
+    {
+        $this->throwExceptionOnShutdown();
+
+        if ($this->isGracefulShutdownDue()) {
+            $this->shutdownGracefully($output);
+        }
+    }
+
+    private function shutdownGracefully(OutputInterface $output): void
+    {
+        $this->shutdown();
+        $this->releaseLock();
+
+        if ($output->getVerbosity() > OutputInterface::VERBOSITY_QUIET) {
+            $output->writeln(sprintf(
+                '<info>Graceful shutting down after %d seconds.</info>',
+                $this->getElapsedTime()
+            ));
+        }
+    }
+
+    private function isGracefulShutdownDue(): bool
+    {
+        $elapsed = $this->getElapsedTime();
+
+        return null !== $this->runtime && null !== $elapsed && $elapsed >= $this->runtime;
+    }
+
+    private function getElapsedTime(): ?int
+    {
+        return null !== $this->startTime ? time() - $this->startTime : null;
+    }
+
+    /**
+     * @noinspection PhpUnused
+     */
+    protected function clearFingersCrossedLogHandlers(mixed $logger): void
+    {
+        if (!(is_a($logger, '\Monolog\Logger'))) {
+            return;
+        }
+
+        foreach ($logger->getHandlers() as $handler) {
+            if (!is_a($handler, '\Monolog\Handler\FingersCrossedHandler')) {
+                continue;
+            }
+
+            $handler->clear();
+        }
+    }
+
+    /**
+     * @noinspection PhpUnusedParameterInspection
+     */
+    protected function createLockKey(InputInterface $input): string
+    {
+        return $this->getName() ?? get_class($this);
+    }
+
+    private function acquireLock(InputInterface $input): void
+    {
+        if ((bool) $input->getOption('run-once')) {
+            return; // No locking needed
+        }
+
+        if (null === $this->lock) {
+            $this->lock = $this->lockFactory->createLock($this->createLockKey($input), ttl: $this->lockTtl);
+        }
+
+        if (!$this->lock->acquire()) {
+            throw new RuntimeException('Another instance is already running.');
+        }
+
+        $this->lock->refresh($this->lockTtl);
+    }
+
+    private function releaseLock(): void
+    {
+        if (null === $this->lock) {
+            return;
+        }
+
+        $this->lock->release();
+    }
+}
